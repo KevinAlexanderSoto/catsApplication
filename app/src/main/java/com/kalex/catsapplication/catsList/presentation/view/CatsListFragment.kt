82bf5bba780d8fa42ce3ba670e8f6ec00d21775f
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-04-14 08:25:14 -0500

    refactor : move CatsListFragment to views folder

--- app/src/main/java/com/kalex/catsapplication/catsList/presentation/view/CatsListFragment.kt
+++ app/src/main/java/com/kalex/catsapplication/catsList/presentation/view/CatsListFragment.kt
@@ -1,38 +1,32 @@
-package com.kalex.catsapplication
+package com.kalex.catsapplication.catsList.presentation.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.kalex.catsapplication.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class CatsListFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
     }
 
     override fun onDestroyView() {

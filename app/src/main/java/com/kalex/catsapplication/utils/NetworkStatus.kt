d876b228c988e8e47272782fd85e672e2593769e
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-04-14 09:25:23 -0500

    feature : add status

---
+++ app/src/main/java/com/kalex/catsapplication/utils/NetworkStatus.kt
@@ -0,0 +1,13 @@
+package com.kalex.catsapplication.utils
+
+sealed class ViewModelNewsUiState<T> {
+    data class Success<T>(val data: T) : ViewModelNewsUiState<T>()
+    data class Loading<T>(val isLoading: Boolean) : ViewModelNewsUiState<T>()
+    data class Error<T>(val exception: Int) : ViewModelNewsUiState<T>()
+}
+
+sealed class UseCaseFlowStatus<T> {
+    data class Success<T>(val data: T) : UseCaseFlowStatus<T>()
+    data class Error<T>(val exception: String) : UseCaseFlowStatus<T>()
+    data class Loading<T>(val message: String) : UseCaseFlowStatus<T>()
+}
